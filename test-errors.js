41c60b0fb2c5c79e275579553453edc555536c76
Srinath <puppala.srinath@gmail.com>
2025-11-21 11:09:35 +0530

    test errors in code

---
+++ test-errors.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs');
+
+function getUserData() {
+  const data = {
+    name: 'John',
+    age: 30
+
+function processUser() {
+  console.log(undefinedVariable);
+}
+
+const config = {
+  host: localhost,
+  port: 3000
+};
+
+function example() {
+  return true;
+  console.log('This will never execute');
+}
+
+var users = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' }
+]
+
+function checkAge(age) {
+  if (age == 18) {
+    return 'Adult';
+  }
+  return 'Minor';
+}
+
+fs.readFile('data.txt', (err, data => {
+  if (err) throw err;
+  console.log(data);
+});
+
+function calculate() {
+  const unusedVar = 42;
+  return 10 + 5;
+}
+
+function emptyFunction()
+
+const message = 'Hello' + + 'World';
